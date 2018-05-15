6c1ab18291ebbfac88f8db4fd8c15be84a2eaf11
rimolive <ricardo.martinelli.oliveira@gmail.com>
2018-05-15 14:10:13 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+US Elections - Machine Learning component
+=========================================
+
+This is a "working in progress" component.
---
+++ src/us-elections.R
@@ -0,0 +1,24 @@
+library(dplyr)
+library(sparklyr)
+
+DF_LOCATION <- "https://raw.githubusercontent.com/rimolive/us-elections/master/data/us-elections.csv"
+
+us.elections_tbl <- copy_to(sc, read.csv(DF_LOCATION))
+
+elections_tbl <- us.elections_tbl %>%
+  group_by(state_name, party) %>%
+  summarise(total_2008 = sum(total_votes_2008),
+            total_2012 = sum(total_votes_2012),
+            total_2016 = sum(total_votes_2016)) %>%
+  arrange(state_name) %>%
+  collect()
+
+elections.2008_tbl <- elections_tbl %>%
+  select(state_name, party, total_2008)
+
+elections.2012_tbl <- elections_tbl %>%
+  select(state_name, party, total_2012)
+
+elections.2016_tbl <- elections_tbl %>%
+  select(state_name, party, total_2016)
+
